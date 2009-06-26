52c63dfe84f6f3f8d24ad3b89ad222ff71a531a0
Jeremy Whitlock <jcscoobyrs@gmail.com>
2009-06-25 20:31:38 -0600

    Update the MainWindow to display profile image for selected tweet user.
    Put in the initial code to start working with the Twitter profile image and
    background code.

--- AppDelegate.h
+++ AppDelegate.h
@@ -16,14 +16,23 @@
 	BOOL isWaiting;
 	NSArray *results;
 	
+	IBOutlet NSTableView *resultsView;
 	IBOutlet NSTextField *userField;
+	IBOutlet NSTextField *userLabel;
 	IBOutlet NSTextField *passField;
 	IBOutlet NSPopUpButton *taskTypeButton;
+	IBOutlet NSImageView *profileImageView;
+	IBOutlet NSButton *changeButton;
 }
 
 @property BOOL isWaiting;
 @property (copy) NSArray *results;
 
 - (IBAction) runTask:(id)sender;
+- (IBAction) findImage:(id)sender;
+
+- (void) runUserTask;
+- (void) changeTwitterProfileImage:(NSString *)imagePath;
+- (NSNumber *) getImageSize:(NSString *)imagePath;
 
 @end
--- AppDelegate.m
+++ AppDelegate.m
@@ -22,6 +22,10 @@
 }
 
 - (IBAction) runTask:(id)sender {
+	[self runUserTask];
+}
+
+- (void) runUserTask {
 	manager.username = [userField stringValue];
 	manager.password = [passField stringValue];
 	
@@ -30,6 +34,61 @@
 	[basicTask run];
 	
 	self.isWaiting = YES;
+}
+
+- (IBAction) findImage:(id)sender {
+	NSOpenPanel *imagePanel = [NSOpenPanel openPanel];
+	NSArray *imageTypes = [NSArray arrayWithObjects:@"gif", @"jpg", @"png", nil];
+	
+	// Only allow a single file to be chosen
+	[imagePanel setCanChooseFiles:YES];
+	[imagePanel setCanChooseDirectories:NO];
+	[imagePanel setAllowsMultipleSelection:NO];
+	
+	// Display the sheet and only allow valid Twitter profile image types (gif, png, jpg)
+	if ([imagePanel runModalForTypes:imageTypes] == NSOKButton) {
+		[self changeTwitterProfileImage:[[imagePanel filenames] objectAtIndex:0]];
+	}
+}
+
+- (void) changeTwitterProfileImage:(NSString *)imagePath {
+	NSLog(@"New image path: %@", imagePath);
+	NSLog(@"New image size: %@K", [self getImageSize:imagePath]);
+	
+	// Verify the image size is under the 700K limit
+	
+	// Change the profile image
+	
+/*	manager.successSelector = @selector(twitterManager:resultsReadyForChangeProfileImageTask:);
+	
+	SDTwitterTask *changeImageTask = [SDTwitterTask taskWithManager:manager];
+	changeImageTask.type = SDTwitterTaskUpdateProfileImage;
+	
+	NSImage *newImage = [[[NSImage alloc] initWithContentsOfFile:imagePath] autorelease];
+
+	changeImageTask.imageToUpload = newImage;
+	
+	[changeImageTask run]; */
+}
+
+- (NSNumber *) getImageSize:(NSString *)imagePath {
+	unsigned long long size = 0;
+	NSFileManager * fm = [NSFileManager defaultManager];
+	NSDictionary *fattrs = [fm fileAttributesAtPath:imagePath traverseLink:NO];
+	
+	size = [[fattrs objectForKey:NSFileSize] unsignedLongLongValue];
+	
+	return [NSNumber numberWithUnsignedLongLong:size / 1024];
+}
+	
+- (void) tableViewSelectionDidChange:(NSNotification *)notification {
+	// Only enable the "Change..." button when the user selected is the
+	// currently logged in user.
+	if ([[manager username] isEqualToString:[userLabel stringValue]]) {
+		[changeButton setEnabled:YES];
+	} else {
+		[changeButton setEnabled:NO];
+	}
 }
 
 - (void) twitterManager:(SDTwitterTaskManager*)manager resultsReadyForTask:(SDTwitterTask*)task {
